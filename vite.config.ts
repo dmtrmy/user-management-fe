10aecd98a7add2f57a76a412c061b745a418db46
Dimitri Mann <dimitrimann@MBP-von-Dimitri.fritz.box>
2025-01-03 18:41:26 +0100

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),  // This is more reliable
+      "@": path.resolve(__dirname, "./src"), // Alias for clean imports
     },
+  },
+  server: {
+    port: 5173, // Explicitly set the development server port
   },
 });
